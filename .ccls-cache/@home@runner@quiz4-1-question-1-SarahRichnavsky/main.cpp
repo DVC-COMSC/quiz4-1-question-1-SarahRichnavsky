2940ee497ee5f426ab4d1422d9ebdb0286df8128
Sarah Richnavsky <112355091+SarahRichnavsky@users.noreply.github.com>
2022-10-06 00:10:12 +0000

    added spaces

--- .ccls-cache/@home@runner@quiz4-1-question-1-SarahRichnavsky/main.cpp
+++ .ccls-cache/@home@runner@quiz4-1-question-1-SarahRichnavsky/main.cpp
@@ -4,47 +4,43 @@
 #include <math.h>
 using namespace std;
 
-int main()
-{
-	int choice;
-	double width, height, radius;
-	double area;
+int main() {
+  int choice;
+  double width, height, radius;
+  double area;
 
-	cout << "Geometry Calculator\n";
-	cout << "\t1. Calculate the Area of Circle\n";
-	cout << "\t2. Calculate the Area of Rectangle\n";
-	cout << "\t3. Calculate the Area of Triangle\n";
-	cout << "\t4. Quit\n\n";
-	cout << "Enter your choice (1-4): ";
-	cin >> choice;
+  cout << "Geometry Calculator\n";
+  cout << "\t1. Calculate the Area of Circle\n";
+  cout << "\t2. Calculate the Area of Rectangle\n";
+  cout << "\t3. Calculate the Area of Triangle\n";
+  cout << "\t4. Quit\n\n";
+  cout << "Enter your choice (1-4): ";
+  cin >> choice;
 
-        //************************
-        // Your code 
-        //************************
-  
-  if (choice == 1){
-    cout<<"Enter radius";
-    cin>>radius; 
-    area = pow(3.14,radius); 
+  //************************
+  // Your code
+  //************************
+
+  if (choice == 1) {
+    cout << "Enter radius ";
+    cin >> radius;
+    area = pow(3.14, radius);
     cout << area << endl;
   }
-  if (choice == 2){
-    cout<<"Enter width and height";
-    cin>> height>> width;
+  if (choice == 2) {
+    cout << "Enter width and height ";
+    cin >> height >> width;
     area = height * width;
-  cout << area << endl;
-    }
-   if (choice == 3){
-     cout<<"Enter width and height";
-    cin>> width>> height;
-    area = width * height *0.5;
-  cout << area << endl;
-     }
-   if (choice == 4){
-     
-    cout <<"Program Stopped"; 
-   }
-    
-    
-        
+    cout << area << endl;
+  }
+  if (choice == 3) {
+    cout << "Enter width and height ";
+    cin >> width >> height;
+    area = width * height * 0.5;
+    cout << area << endl;
+  }
+  if (choice == 4) {
+
+    cout << "Program Stopped";
+  }
 }
